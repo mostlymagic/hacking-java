2c99c80a19f04f1b4e1131554a7862867b8d415f
Sean Patrick Floyd <Sean.Floyd@zalando.de>
2016-01-21 11:50:10 +0100

    Reformat

--- 3_defect-analysis/3_aspectj/src/main/java/org/zalando/techtalks/hackingjava/defectanalysis/aspectj/AspectJPolicyEnforcementEngine.java
+++ 3_defect-analysis/3_aspectj/src/main/java/org/zalando/techtalks/hackingjava/defectanalysis/aspectj/AspectJPolicyEnforcementEngine.java
@@ -1,25 +1,27 @@
 package org.zalando.techtalks.hackingjava.defectanalysis.aspectj;
 
 import java.io.File;
-
 import org.aspectj.tools.ajc.Main;
-
 import org.zalando.techtalks.hackingjava.common.compiler.CompilationResult;
 import org.zalando.techtalks.hackingjava.common.compiler.ForkedRun;
 import org.zalando.techtalks.hackingjava.defectanalysis.baseline.engine.DefectAnalysisEngine;
 
-/**
- * @author  Sean Patrick Floyd (sean.floyd@zalando.de)
- * @since   21.01.2016
- */
+import static java.lang.String.format;
+
 public class AspectJPolicyEnforcementEngine implements DefectAnalysisEngine {
+
+    private static final String COMPLIANCE_LEVEL = "1.8";
+
     @Override
     public CompilationResult compile(final File sourceFile) {
         final File aspectFile = findAspectFile(sourceFile);
-        return new ForkedRun(AspectJPolicyEnforcementEngine.class).withArg(Main.class).withArg("-1.8")
-                                                                  .withArg("-target").withArg("1.8")
-                                                                  .withArg(sourceFile.getAbsolutePath())
-                                                                  .withArg(aspectFile.getAbsolutePath()).run();
+        return new ForkedRun(AspectJPolicyEnforcementEngine.class)
+                .withArg(Main.class)
+                .withArg(format("-%s", COMPLIANCE_LEVEL))
+                .withArg("-target").withArg(COMPLIANCE_LEVEL)
+                .withArg(sourceFile.getAbsolutePath())
+                .withArg(aspectFile.getAbsolutePath())
+                .run();
     }
 
     private File findAspectFile(final File sourceFile) {
