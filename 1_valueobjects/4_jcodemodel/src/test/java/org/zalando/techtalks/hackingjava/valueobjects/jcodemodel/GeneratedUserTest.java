3c0779de22cdbf66fffeb243d740c653dbce6235
Sean Patrick Floyd <Sean.Floyd@zalando.de>
2016-01-26 17:44:25 +0100

    Being more explicit about return type

--- 1_valueobjects/4_jcodemodel/src/test/java/org/zalando/techtalks/hackingjava/valueobjects/jcodemodel/GeneratedUserTest.java
+++ 1_valueobjects/4_jcodemodel/src/test/java/org/zalando/techtalks/hackingjava/valueobjects/jcodemodel/GeneratedUserTest.java
@@ -4,7 +4,7 @@ import com.google.common.collect.ImmutableList;
 import org.zalando.techtalks.hackingjava.valueobjects.tests.BaseUserTest;
 
 public class GeneratedUserTest extends BaseUserTest {
-    @Override protected Object createUser() {
+    @Override protected User createUser() {
         final Address address = new Address(STREET, ZIP_CODE, CITY);
         return new User(FIRST_NAME, LAST_NAME, BIRTH_DATE, ImmutableList.of(address));
     }
